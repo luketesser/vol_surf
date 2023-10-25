a8eb7e32afe270228e3f340e1e2980a289c461a2
Lucas Tesser <lcstesser@gmail.com>
2023-10-25 10:28:31 -0300

    Addresses issues #1, #2, #3, #4, #5.

--- R/vol_coef.R
+++ R/vol_coef.R
@@ -5,17 +5,33 @@
 #' @param du_mat Time to maturity.
 #' @param implicit_vol Observed volatility from trading derivatives of the underlying asset.
 #'
-#' @return
+#' @return Two Coefficients that represent the skew and the kurtosis of the smile curve.
 #' @export
 #'
 #' @examples
 vol_coef <- function(strike, under_price, du_mat, implicit_vol){
-  v1 <- 10*log(strike/under_price)
+  
+  assertthat::assertthat(assertthat::is.number(strike))
+  
+  assertthat::assertthat(assertthat::is.number(under_price))
+  
+  assertthat::assertthat(assertthat::is.number(du_mat))
+  
+  assertthat::assertthat(assertthat::is.number(implicit_vol))
+  
+  v1 <- 10 * log(strike/under_price)
+  
   v2 <- v1^2
-  v3 <- log(strike/under_price)/sqrt(du_mat/252)
+  
+  v3 <- log(strike / under_price) / sqrt(du_mat / 252)
+  
   v4 <- base::ifelse(strike < under_price, 1.1, 1)
-  v5 <- 10*v4*atan(v3/v4)
+  
+  v5 <- 10 * v4 * atan(v3 / v4)
+  
   v6 <- v5^2
-  v7 <- lm(implicit_vol ~ v5 + v6)$coefficients
+  
+  v7 <- stats::lm(implicit_vol ~ v5 + v6)$coefficients
+  
   return(v7)
 }
